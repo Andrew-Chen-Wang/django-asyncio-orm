b7f24100a87945d02a01c1dd5f47de65bf638c96
Andrew-Chen-Wang <acwangpython@gmail.com>
2022-02-12 01:33:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Django Asyncio ORM
+
+By: [Andrew-Chen-Wang](https://github.com/Andrew-Chen-Wang)
+
+Bringing native asyncio support to Django ORM. You can track the PR
+in Django core [here]().
+
+The purpose of this repository is to demonstrate a proof of concept and,
+in the future, PR the asyncio engine with aiosqlite, merge async ORM code, and
+experiment and discuss the structure of asyncio in Django.
+
+### License and Credit
+
+This repository is licensed under Apache 2.0.
+
+Implementation started sometime in November 2021; package created on 12
+February 2022 to experiment.
---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import find_packages, setup
+
+long_desc = (
+    open("README.md", "rb").read().decode("utf-8")
+    + "\n\n"
+    + open("CHANGELOG.md", "rb").read().decode("utf-8")
+)
+
+setup(
+    name="django-asyncio-orm",
+    version="0.1.0",
+    description="Django native async ORM.",
+    long_description=long_desc,
+    long_description_content_type="text/x-rst",
+    author="Andrew Chen Wang",
+    author_email="acwangpython@gmail.com",
+    license="Apache License 2.0",
+    url="http://github.com/Andrew-Chen-Wang/django-asyncio-orm",
+    packages=find_packages(),
+    install_requires=["django"],
+    tests_require=["tox"],
+    keywords=["django", "async", "async ORM", "ORM", "asyncio", "aiosqlite"],
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    zip_safe=False,
+)
